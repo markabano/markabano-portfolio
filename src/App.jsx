5452a0358acfa71fe35997ab2f5c0bef55d16443
markabano <markabanomain@gmail.com>
2025-10-20 15:40:18 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <nav className='flex justify-center items-center'>
+        <ul className='flex flex-row gap-80'>
+          <li><a href="http://">Home</a></li>
+          <li><a href="http://">About Me</a></li>
+          <li><a href="http://">Contact</a></li>
+        </ul>
+      </nav>
+
+      <section>
+        <div>
+          <h1>HI! I'M MARK JOSEPH E. ABAÑO</h1>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
