331d801b6888c82962eaec3e999f43c0aa85bca5
markabano <markabanomain@gmail.com>
2025-10-20 16:58:02 +0800

    wip: intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <nav className='flex justify-center items-center'>
-        <ul className='flex flex-row gap-80'>
-          <li><a href="http://">Home</a></li>
-          <li><a href="http://">About Me</a></li>
-          <li><a href="http://">Contact</a></li>
-        </ul>
-      </nav>
+    <div className='flex flex-col items-center'>
+      <div className='flex justify-center'>
+        <nav className='flex justify-center bg-red-800 w-5xl rounded-full'>
+          <ul className='flex flex-row text-white items-center gap-80 h-10'>
+            <li><a href="http://">Home</a></li>
+            <li><a href="http://">About Me</a></li>
+            <li><a href="http://">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <section className='grid grid-cols-2 gap-4 w-7xl mt-5 bg-gray-400 rounded-4xl p-5'>
+        <div className='grid grid-rows'>
+          <h1 className='text-8xl'>MARK JOSEPH E. ABANO</h1>
+          <p>
+            Hi, I’m Mark Joseph Abano, a passionate web developer who loves building modern, efficient, and user-friendly web applications.
+            I enjoy turning ideas into functional digital experiences using technologies like React, TypeScript, and Node.js.
+            My goal is to create solutions that are not only visually appealing but also impactful and maintainable.
+          </p>
+        </div>
+        <div className='flex justify-center'>
+          <img src="./public/imgs/profile.jpeg" alt="Profile" className='h-130' />
+        </div>
+      </section>
 
       <section>
         <div>
-          <h1>HI! I'M MARK JOSEPH E. ABAÑO</h1>
+          <h1>Projects</h1>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h1>Contacts</h1>
         </div>
       </section>
     </div>
